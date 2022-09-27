745e0adb833a4bd03b55ee15e6b49d2a6b86c3da
greatlaki <greatlaki911@gmail.com>
2022-09-27 14:25:21 +0300

    function get data of product

--- src/scrapy_articles/views.py
+++ src/scrapy_articles/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+import requests
+from rest_framework import status
 
-# Create your views here.
+
+def get_product_data(article):
+
+    response = requests.get(f'https://basket-01.wb.ru/vol{str(article)[:3]}/part{str(article)[:5]}/{article}/info/ru/card.json')
+    if response.status == status.HTTP_200_OK:
+        result = response.json()
+        article = result.get('nm_id')
+        brand = result.get('selling').get('brand_name')
+        title = result.get('imt_name')
+    return {'Article': article, 'Brand': brand, 'Title': title}
